40f07e89855349454f6a414f2d0d263f952d010e
RHEANNA ALIMO <rheanna.alimo@outlook.com>
2018-11-18 14:13:42 -0800

    Added MorseCode Solution & Tests

---
+++ MorseCode/MorseEncoder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    public class MorseEncoder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Encoder("Hello World"));
+        }
+
+        public static string Encoder(string message)
+        {
+            Dictionary<char, string> morseDict = new Dictionary<char, string>
+            {
+                { '!', "...---..." },
+                { '.', "-.-.--" },
+                { 'A', ".-.-.-" },
+                { 'B', ".-" },
+                { 'C', "-..." },
+                { 'D', "-.-." },
+                { 'E', "-.." },
+                { 'F', "..-." },
+                { 'G', "--." },
+                { 'H', "...." },
+                { 'I', ".." },
+                { 'J', ".---" },
+                { 'K', "-.-" },
+                { 'L', ".-.." },
+                { 'M', "--" },
+                { 'N', "-." },
+                { 'O', "---" },
+                { 'P', ".--." },
+                { 'Q', "--.-" },
+                { 'R', ".-." },
+                { 'S', "..." },
+                { 'T', "-" },
+                { 'U', "..-" },
+                { 'V', "...-" },
+                { 'W', ".--" },
+                { 'X', "-..-" },
+                { 'Y', "-.--" },
+                { 'Z', "--.." }
+            };
+
+            string result = "";
+            message = message.ToUpper();
+
+            char[] toEncode = message.ToCharArray();
+            for (int i = 0; i < toEncode.Length; i++)
+            {
+                if (morseDict.ContainsKey(toEncode[i]) == true)
+                {
+                    result += " " + morseDict[toEncode[i]];
+                }
+                else result += "  ";
+            }
+
+            return result.Trim();
+        }
+    }
+}
---
+++ MorseCodeTests/MorseEncoderTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MorseCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode.Tests
+{
+    [TestClass()]
+    public class MorseEncoderTests
+    {
+        [TestMethod()]
+        public void EncoderTest()
+        {
+            Assert.AreEqual(".... -.. .-.. .-.. ---   .-- --- .-. .-.. -.-.", MorseEncoder.Encoder("Hello world"));
+        }
+    }
+}
